c055e08c52d74a7a953429c6f9366ee89a6190cc
Daniel-dvlp <daniel_clw@hotmail.com>
2025-06-24 21:01:15 -0500

    subida nueva de archivos a mi rama

--- src/features/dashboard/pages/products/ProductsPage.jsx
+++ src/features/dashboard/pages/products/ProductsPage.jsx
@@ -1,3 +1,4 @@
+// src/dashboard/pages/work_scheduling/WorkSchedulingPage.jsx
 import React from 'react';
 
 const ProductsPage = () => {
@@ -5,9 +6,9 @@ const ProductsPage = () => {
     <div>
       <h1 className="text-3xl font-bold text-gray-800">Productos</h1>
       <p className="mt-4 text-gray-600">
-        Aquí se mostrará el modulo de .
+        Aquí se mostrará el modulo de Productos.
       </p>
-      
+      {/* En el futuro, aquí irá tu componente de Calendario */}
     </div>
   );
 };
