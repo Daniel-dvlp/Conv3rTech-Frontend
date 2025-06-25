5ac5a59471cf14cf81ef279c9f6d79aff1c82b19
Daniel-dvlp <daniel_clw@hotmail.com>
2025-06-24 21:11:29 -0500

    Se eliminan archivos duplicados que generan conflicto, se valida el commit

--- src/features/dashboard/pages/products_category/ProductsCategoryPage.jsx
+++ src/features/dashboard/pages/products_category/ProductsCategoryPage.jsx
@@ -1,13 +1,13 @@
+// src/dashboard/pages/work_scheduling/WorkSchedulingPage.jsx
 import React from 'react';
 
 const ProductsCategoryPage = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-gray-800">Categoría de Productos</h1>
+      <h1 className="text-3xl font-bold text-gray-800">Categoria de produtos</h1>
       <p className="mt-4 text-gray-600">
-        Aquí se mostrará el modulo de .
+        Aquí se mostrará el modulo de Categoria de productos.
       </p>
-      
     </div>
   );
 };
--- src/features/dashboard/pages/quotes/QuotesPage.jsx
+++ src/features/dashboard/pages/quotes/QuotesPage.jsx
@@ -1,3 +1,4 @@
+// src/dashboard/pages/work_scheduling/WorkSchedulingPage.jsx
 import React from 'react';
 
 const QuotesPage = () => {
@@ -5,9 +6,9 @@ const QuotesPage = () => {
     <div>
       <h1 className="text-3xl font-bold text-gray-800">Cotizaciones</h1>
       <p className="mt-4 text-gray-600">
-        Aquí se mostrará el modulo de .
+        Aquí se mostrará el modulo de Cotizaciones.
       </p>
-      
+      {/* En el futuro, aquí irá tu componente de Calendario */}
     </div>
   );
 };
