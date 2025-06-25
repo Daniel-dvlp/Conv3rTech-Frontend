f2184511d7e62ba921ca94f381c694cd90146685
Daniel-dvlp <daniel_clw@hotmail.com>
2025-06-24 21:04:05 -0500

    subida nueva de archivos a mi rama, para reslucion de conflictos

---
+++ src/features/dashboard/pages/Quotes/QuotesPage.jsx
@@ -0,0 +1,16 @@
+// src/dashboard/pages/work_scheduling/WorkSchedulingPage.jsx
+import React from 'react';
+
+const QuotesPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-gray-800">Citas</h1>
+      <p className="mt-4 text-gray-600">
+        Aquí se mostrará el modulo de Citas.
+      </p>
+      
+    </div>
+  );
+};
+
+export default QuotesPage;
